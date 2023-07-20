3a3c4e24f5fe2c9055d1237ec01c39b35787c3a2
Owsiej <owsiejaszek@gmail.com>
2023-07-20 13:19:39 +0200

    finished views with templates

--- flat_browser/models.py
+++ flat_browser/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
+
 class Developer(models.Model):
     name = models.CharField(max_length=100, unique=True)
     url = models.URLField()
@@ -9,7 +12,7 @@ class Developer(models.Model):
     last_modified = models.DateTimeField(auto_now=True, editable=False, blank=True)
 
     def __str__(self):
-        return f"Developer: {self.name}"
+        return self.name
 
     class Meta:
         ordering = ['name']
@@ -24,22 +27,33 @@ class Investment(models.Model):
     last_modified = models.DateTimeField(auto_now=True, editable=False, blank=True)
 
     def __str__(self):
-        return f"Investment: {self.name}, {self.developer}"
+        return self.name
 
     class Meta:
-        order_with_respect_to = 'developer'
+        ordering = ['developer', 'name']
         select_on_save = True
 
 
 class Flat(models.Model):
+    FLAT_STATUS = [('wolne', 'wolne'),
+                   ('sprzedane', 'sprzedane'),
+                   ('zarezerwowane', 'zarezerwowane')]
+
     investment = models.ForeignKey("Investment", on_delete=models.CASCADE)
-    floor = models.IntegerField()
-    rooms = models.IntegerField()
-    area = models.FloatField()
-    price = models.FloatField()
-    status = models.CharField(max_length=50)
+    developer = models.ForeignKey("Developer", on_delete=models.CASCADE)
+    floor = models.PositiveIntegerField()
+    rooms = models.PositiveIntegerField(validators=[MaxValueValidator(10)])
+    area = models.FloatField(validators=[MinValueValidator(0.00)])
+    price = models.FloatField(validators=[MinValueValidator(0.00)])
+    status = models.CharField(max_length=50, choices=FLAT_STATUS)
     url = models.URLField()
     insertion_date = models.DateField(auto_now_add=True, editable=False, blank=True)
 
     def __str__(self):
         return f"Flat: {self.investment}"
+
+    def clean(self):
+        if self.investment.developer.id != self.developer.id:
+            raise ValidationError({
+                "developer": "Developer should be same as in investment."
+            })
